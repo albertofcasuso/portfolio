60321db85c3740820f0eb9170d695f5e2eff0319
Casuso <albertofcasuso@gmail.com>
2023-11-30 08:38:33 +0100

    fixes

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
 import Router from './routes';
-import './index.css';
 import store from './redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Provider store={store}>
-    <Router />
-  </Provider>
+  <ChakraProvider>
+    <Provider store={store}>
+      <Router />
+    </Provider>
+  </ChakraProvider>
 );
---
+++ vite.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          redux: ['@reduxjs/toolkit', 'react-redux'],
+          chakra: ['@chakra-ui/react'],
+          emotion: ['@emotion/styled', '@emotion/react']
+        }
+      }
+    }
+  }
+});
