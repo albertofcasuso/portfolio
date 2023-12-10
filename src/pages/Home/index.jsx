85b94fe3f88730692bff57e0ffc3ad735335a4ef
Casuso <albertofcasuso@gmail.com>
2023-12-10 14:46:08 +0100

    fixes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,23 +1,59 @@
 import React, { useState, useEffect } from 'react';
 import LightBox from 'yet-another-react-lightbox';
+import { Masonry, useInfiniteLoader } from 'masonic';
 import Fullscreen from 'yet-another-react-lightbox/plugins/fullscreen';
 import 'yet-another-react-lightbox/plugins/thumbnails.css';
 import 'yet-another-react-lightbox/styles.css';
-import { Spinner, Box, Image, Fade, AbsoluteCenter } from '@chakra-ui/react';
+import { Spinner, Box, Image, Fade, Text, AbsoluteCenter } from '@chakra-ui/react';
 import { useGetFotoMediaQuery } from '../../services/media';
 
 export default function Home() {
+  const [page, setPage] = useState(1);
   const [index, setIndex] = useState(-1);
-  const [photoSrc, setPhotoSrc] = useState([]);
-  const { data, isLoading } = useGetFotoMediaQuery();
-  useEffect(() => {
-    if (!isLoading && data && data.length > 0) {
-      const photosSrc = data.map(item => {
-        return { src: item.source_url, width: item.media_details.width, height: item.media_details.height };
-      });
-      setPhotoSrc(photosSrc);
+  const { data, isLoading, isFetching, isError } = useGetFotoMediaQuery(page);
+  const photos = data ? data : [];
+  const lightboxPhotos = data
+    ? data.map(item => ({
+        src: item.source_url
+      }))
+    : [];
+
+  const onScroll = () => {
+    const scrolledToBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight - 400;
+    if (scrolledToBottom && !isFetching) {
+      if (!isError) {
+        setPage(page + 1);
+      }
     }
-  }, [data, isLoading]);
+  };
+
+  useEffect(() => {
+    document.addEventListener('scroll', onScroll);
+
+    return function () {
+      document.removeEventListener('scroll', onScroll);
+    };
+  }, [page, isFetching]);
+
+  const handleImageClick = index => {
+    setIndex(index);
+  };
+
+  const MasonryCard = ({ index, data: { source_url }, width }) => {
+    return (
+      <>
+        <Image
+          key={source_url}
+          w="100%"
+          mb={2}
+          src={source_url}
+          alt="Alt"
+          onClick={() => handleImageClick(index)}
+          loading="lazy"
+        />
+      </>
+    );
+  };
 
   if (isLoading) {
     return (
@@ -33,36 +69,14 @@ export default function Home() {
   if (!isLoading && data) {
     return (
       <>
-        <Box padding={4} w="100%" mx="auto" sx={{ columnCount: [1, 2, 2], columnGap: '8px' }}>
-          {photoSrc.map((src, index) => {
-            return (
-              <Fade in={src.src} key={src.src}>
-                <Image
-                  key={src.src}
-                  w="100%"
-                  mb={2}
-                  d="inline-block"
-                  src={src.src}
-                  alt="Alt"
-                  onClick={() => setIndex(index)}
-                />
-              </Fade>
-            );
-          })}
-        </Box>
-        {/* <PhotoAlbum
-          layout="masonry"
-          photos={photoSrc}
-          onClick={({ index }) => setIndex(index)}
-          columns={containerWidth => {
-            if (containerWidth >= 300 && containerWidth < 600) return 2;
-            if (containerWidth >= 600) return 2;
-          }}
-          componentsProps={containerWidth => ({
-            imageProps: { loading: 'lazy' }
-          })}
-        /> */}
-        <LightBox slides={photoSrc} open={index >= 0} index={index} close={() => setIndex(-1)} plugins={[Fullscreen]} />
+        <Masonry items={photos} render={MasonryCard} maxColumnCount={2} columnGutter={10} rowGutter={5} />
+        <LightBox
+          slides={lightboxPhotos}
+          open={index >= 0}
+          index={index}
+          close={() => setIndex(-1)}
+          plugins={[Fullscreen]}
+        />
       </>
     );
   }
--- src/services/media.js
+++ src/services/media.js
@@ -3,10 +3,20 @@ import { emptyApi } from './apiSetup';
 export const mediaApi = emptyApi.injectEndpoints({
   endpoints: build => ({
     getFotoMedia: build.query({
-      query: () => {
-        return { url: 'media?parent=22&per_page=20' };
+      query: (page = 1) => {
+        return { url: `media?parent=22&page=${page}&per_page=5` };
       },
-      providesTags: ['Media']
+      serializeQueryArgs: ({ endpointName }) => {
+        return endpointName;
+      },
+      // Always merge incoming data to the cache entry
+      merge: (currentCache, newItems) => {
+        currentCache.push(...newItems);
+      },
+      // Refetch when the page arg changes
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
+      }
     })
   }),
   overrideExisting: false
